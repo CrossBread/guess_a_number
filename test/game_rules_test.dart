cdeeb3bb24c131a02afe87b7c1fa027886cdbff1
Jeff <jeff.neet@gmail.com>
2023-06-30 14:37:41 -0600

    Game Rules Test Skeleton

---
+++ test/game_rules_test.dart
@@ -0,0 +1,21 @@
+// Test class for game rules
+
+import 'package:test/test.dart';
+
+main() {
+  group('Game Rules', () {
+    test('Guess is correct', () {});
+    test('Guess is high', () {});
+    test('Guess is low', () {});
+    test('Guess is negative', () {});
+    test('Guess is out of bounds', () {});
+    test('Guess limit is reached', () {});
+    test('AI Guess is correct', () {});
+    test('AI Guess is high', () {});
+    test('AI Guess is low', () {});
+    test('New game state', () {});
+    test('Play again state is reset', () {});
+    test('Number of tries is correct when Player wins', () {});
+    test('Number of tries is correct when AI wins', () {});
+  });
+}
